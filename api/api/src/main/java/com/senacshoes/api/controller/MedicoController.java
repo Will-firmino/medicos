1780a647f31726abcb7fbb3442eb4ea9b074fc2b
Doutor William <113191721+Will-firmino@users.noreply.github.com>
2024-08-19 21:03:40 -0300

    Add files via upload

--- api/api/src/main/java/com/senacshoes/api/controller/MedicoController.java
+++ api/api/src/main/java/com/senacshoes/api/controller/MedicoController.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.senacshoes.api.model.medico.DadosAtualizarMedico;
 import com.senacshoes.api.model.medico.DadosCadastroMedico;
 import com.senacshoes.api.model.medico.DadosListagemMedico;
 import com.senacshoes.api.model.medico.Medico;
@@ -33,7 +37,17 @@ public class MedicoController {
 		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
 	}
 	
+	@PutMapping
+	@Transactional
+	public void atualizar(@RequestBody DadosAtualizarMedico dados) {
+    var medico = repository.getReferenceById(dados.id());
+    medico.atualizarInformacoes(dados);
+	}
 
-
+  @DeleteMapping("/{id}")
+  @Transactional
+  public void excluir(@PathVariable Long id) {
+    repository.deleteById(id);
+  }
 
 }
--- api/api/src/main/java/com/senacshoes/api/model/medico/DadosListagemMedico.java
+++ api/api/src/main/java/com/senacshoes/api/model/medico/DadosListagemMedico.java
@@ -1,10 +1,15 @@
 package com.senacshoes.api.model.medico;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidades especialidade) {
+public record DadosListagemMedico(
+    Long id,
+    String nome, 
+    String email, 
+    String crm, 
+    Especialidades especialidade) {
    
 
     public DadosListagemMedico(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
     
 
